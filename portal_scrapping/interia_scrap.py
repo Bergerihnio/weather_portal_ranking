d59f9ae2eb820d44352931e7f7a2b813b2b3abc3
Bergerihnio <filipekbergerek@protonmail.com>
2024-07-14 05:18:05 +0200

    Fetch emoji of weather to db

--- portal_scrapping/interia_scrap.py
+++ portal_scrapping/interia_scrap.py
@@ -28,22 +28,72 @@ def scrap():
         formatted_temp = temp.replace("°C", "")
         temp_list.append(formatted_temp)
 
-    data = merge_data(hours_list, temp_list)
+    list_behave = scrap_behavior()
+
+    data = merge_data(hours_list, temp_list, list_behave)
 
     insert_into_db(data)
 
-def merge_data(hours_list, temp_list):
+def merge_data(hours_list, temp_list, list_behave):
+
     data = []
 
-    for score in zip(hours_list, temp_list):
+    for score in zip(hours_list, temp_list, list_behave):
         data.append(score)
     
     return data
 
+def scrap_behavior():
+    r = requests.get('https://pogoda.interia.pl/pogoda-pojutrze-blonie,cId,1689')
+    if r.status_code != 200:
+        return 
+    
+    soup = BeautifulSoup(r.content, 'html.parser')
+    find_behave = soup.find_all('span', class_='forecast-icon')
+    
+    list_behave = []
+
+    for title in find_behave:
+        title_text = title.get('title')
+
+        match title_text:
+            case 'Słonecznie':
+                title_text = '☀️'
+            case 'Przeważnie słonecznie':
+                title_text = '🌤️'
+            case 'Częściowo słonecznie':
+                title_text = '⛅'
+            case 'Przejściowe zachmurzenie':
+                title_text = '🌥️'
+            case 'Bezchmurnie':
+                title_text = '🔵'
+            case 'Zachmurzenie duże':
+                title_text = '☁️'
+            case 'Zachmurzenie małe':
+                title_text = '☁️'
+            case 'Zachmurzenie umiarkowane':
+                title_text = '☁️'
+            case 'Pochmurno':
+                title_text = '☁️' 
+            case 'Deszcz':
+                title_text = '🌧️'
+            case 'Przelotne opady':
+                title_text = '🌦️'
+            case 'Burze z piorunami':
+                title_text = '⛈️'
+            case 'Częściowo słonecznie i burze z piorunami':
+                title_text = '🌦️⛈️'
+            case _:
+                title_text = '❓'
+
+        list_behave.append(title_text)
+
+    return list_behave
+
 def insert_into_db(data):
     conn = sqlite3.connect('interia_temperatures.db')
     c = conn.cursor()
-
+    
     for element in data:
         time = element[0]
         int_time = int(time)
@@ -52,12 +102,16 @@ def insert_into_db(data):
         
         formatted_time = f'{int_time}:00'
         temp = element[1]
+        emoji = element[2]
 
-        c.execute("INSERT INTO interia_temperatures (temperature, time) VALUES (?, ?)",
-                (temp, formatted_time))
+        c.execute("INSERT INTO interia_temperatures (temperature, time, emoji) VALUES (?, ?, ?)",
+                (temp, formatted_time, emoji))
         conn.commit()
     conn.close()
 
 
+
 if __name__ == '__main__':
     scrap()
+
+
--- portal_scrapping/interia_sqlite.py
+++ portal_scrapping/interia_sqlite.py
@@ -7,6 +7,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS interia_temperatures
         (temperature INTEGER,
         time TEXT DATETIME,
+        emoji TEXT,
         date TEXT DEFAULT (date('now', '+2 days')),
         currently_date TEXT DEFAULT (date('now')))''')
 
